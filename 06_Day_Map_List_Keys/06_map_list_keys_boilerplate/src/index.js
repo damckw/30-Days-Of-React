cad8beca560cc7443e799c89038303894cb5a547
Damien Chan <damien.chan@epitech.eu>
2022-07-06 15:34:22 +0000

    DAY 06

--- 06_Day_Map_List_Keys/06_map_list_keys_boilerplate/src/index.js
+++ 06_Day_Map_List_Keys/06_map_list_keys_boilerplate/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import './index.css'
 
 // importing data
-
+// eslint-disable-next-line
 import { countriesData } from './data/countries'
+// eslint-disable-next-line
 import { tenMostHighestPopulations } from './data/ten_most_highest_populations'
 
 const countries = [
@@ -33,14 +34,45 @@ const Countries = ({ countries }) => {
   return <div>{countryList}</div>
 }
 
+function isPrime(num) {
+  for(var i = 2; i < num; i++)
+    if(num % i === 0) return false;
+  return num > 1;
+}
+
+const Number = ({number}) => {
+  return (
+    <div style={{backgroundColor: number % 2 === 0 ? "green" : (isPrime(number) ? "red" : "yellow")}}>
+      <h1>{number}</h1>
+    </div>
+  )
+}
+
+const numberContainer = {
+  display: "grid"
+}
+
+const Numbers = ({ numbers }) => {
+  const numberList = numbers.map((number) => (
+    <Number key={number} number={number} />
+  ))
+
+  return <div className='numberContainer'>{numberList}</div>
+}
+
+const numbers = [...Array(32).keys()]
+
+
 // The App, or the parent or the container component
 // Functional Component
 const App = () => {
   return (
     <div className='app'>
-      <div>
-        <h1>Countries List</h1>
-        <Countries countries={countries} />
+      <div style={{textAlign: "center"}}>
+        <h1>30 Days of React</h1>
+        <h2>Number Generator</h2>
+        <Numbers numbers={numbers}></Numbers>
+        {/* <Countries countries={countries} /> */}
       </div>
     </div>
   )
